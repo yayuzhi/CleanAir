60fba25e45dc9be8d36a9228d9bcb3611050fb14
yayuzhi <3229846971@qq.com>
2021-01-18 13:12:43 +0800

    准备sso

--- cleanair/cleanair-manage/src/main/java/com/ca/CleanairManageApplication.java
+++ cleanair/cleanair-manage/src/main/java/com/ca/CleanairManageApplication.java
@@ -7,8 +7,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
-@EnableAsync
-@EnableCaching //启动缓存配置(由springboot帮你完成自动配置)
 @SpringBootApplication
 @MapperScan("com.ca.mapper")
 public class CleanairManageApplication {
--- cleanair/cleanair-web/src/main/java/com/ca/CleanAirWebApplication.java
+++ cleanair/cleanair-web/src/main/java/com/ca/CleanAirWebApplication.java
@@ -3,14 +3,12 @@ package com.ca;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
-@EnableAsync
-@EnableCaching //启动缓存配置(由springboot帮你完成自动配置)
-@SpringBootApplication
-@MapperScan("com.ca.mapper")
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CleanAirWebApplication {
 
     public static void main(String[] args) {
